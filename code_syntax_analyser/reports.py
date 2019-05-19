573ab16a84bec6c70d331bff21dc205474d98159
ndrwpvlv <ndrw.pvlv@gmail.com>
2019-05-19 19:01:39 +0300

    Update of saving reports procedure. Now it saves reports to path with clones git repo

--- code_syntax_analyser/reports.py
+++ code_syntax_analyser/reports.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import json
+import os
 
 from jinja2 import Template
 
-from .helpers import Filters
+from .helpers import Filters, path_format
 
 
 class Report(Filters):
@@ -12,17 +13,19 @@ class Report(Filters):
     Basic class of reports generator
     """
 
-    def __init__(self, data: list, filename: str, template_jinja: str):
+    def __init__(self, data: list, filename: str, template_jinja: str, path: str = os.getcwd(), ):
         """
 
         :param data: List of data for report processing, list
         :param filename: Filename for saving files, str
         :param template_jinja: Jinja2 template in string, str
+        :param pat
         """
         super(Report, self).__init__()
         self.data = data
         self.filename = filename
         self.template = template_jinja
+        self.path = path_format(path)
         self.filter_add('console', self.console_log)
         self.filter_add('json', self.json_write)
         self.filter_add('csv', self.csv_write)
@@ -35,15 +38,15 @@ class Report(Filters):
 
     def txt_write(self):
         template = Template(self.template)
-        with open('{}.txt'.format(self.filename), 'w') as fw:
+        with open('{}{}.txt'.format(self.path, self.filename), 'w') as fw:
             fw.write(template.render(data=self.data))
 
     def json_write(self):
-        with open('{}.json'.format(self.filename), 'w') as fw:
+        with open('{}{}.json'.format(self.path, self.filename), 'w') as fw:
             fw.write(json.dumps(self.data))
 
     def csv_write(self):
-        with open('{}.csv'.format(self.filename), 'w') as fw:
+        with open('{}{}.csv'.format(self.path, self.filename), 'w') as fw:
             fw.writelines('{}\n'.format(','.join(map(str, line))) for line in self.data)
 
     def reports_gen_all(self):
