b28220eea4a8930d25dd6720b1621f14e921d02d
ndrwpvlv <ndrw.pvlv@gmail.com>
2019-05-24 11:28:31 +0300

    Update variables names, functions names, imports, default url for git, modify variables setup for classes init,

--- code_syntax_analyser/__main__.py
+++ code_syntax_analyser/__main__.py
@@ -6,8 +6,8 @@ import os
 import sys
 
 from code_syntax_analyser.config import *
-from code_syntax_analyser.helpers import git_clone, url_check, extensions_format, directory_cleanup, path_format, \
-    directory_temp_create, directory_check_exist_and_permissions
+from code_syntax_analyser.helpers import git_clone, check_url, format_extensions, directory_cleanup, format_path, \
+    create_temp_directory, check_directory_exist_and_permissions, filter_list
 from code_syntax_analyser.parsers import CodeSource
 from code_syntax_analyser.reports import Report
 from code_syntax_analyser.texts import TextTokenizer, WordsStatistic
@@ -16,7 +16,7 @@ from code_syntax_analyser.texts import TextTokenizer, WordsStatistic
 def args_get() -> dict:
     parser = argparse.ArgumentParser(prog='code_syntax_analyser',
                                      description='Counting of words in functions, classes and variables names', )
-    parser.add_argument('-g', '--git_url', default='https://github.com/ndrwpvlv/code_analyser',
+    parser.add_argument('-g', '--git_url', default='https://github.com/ndrwpvlv/code_syntax_analyser',
                         help="URL of Git repository", type=str)
     parser.add_argument('-p', '--path', default=os.getcwd(), help="Directory path for code analysis", type=str)
     parser.add_argument('-f', '--files_number_limit', default=FILES_NUMBER_LIMIT,
@@ -31,7 +31,7 @@ def args_get() -> dict:
                         help="Type of words: 'verb' - verbs, 'noun' - nouns, 'all' - all", type=str)
     parser.add_argument('-s', '--syntax_type', default='all',
                         help="Type of syntax: 'class' - classes, 'function' - functions, 'variable' - variables, "
-                             "'a' - all", type=str)
+                             "'all' - all", type=str)
     parser.add_argument('-r', '--report_type', default='all',
                         help="Type of report: 'json', 'csv', 'txt', 'console', 'all'", type=str)
     parser.add_argument('-c', '--cleanup', action='store_true', help='Use temporary directory and cleanup it',
@@ -40,15 +40,17 @@ def args_get() -> dict:
     args = parser.parse_args(sys.argv[1:])
 
     return {
-        'git_url': url_check(args.git_url),
-        'path': path_format(args.path) if directory_check_exist_and_permissions(args.path) else sys.exit(errno.EACCES),
+        'git_url': check_url(args.git_url),
+        'path': format_path(args.path) if check_directory_exist_and_permissions(args.path) else sys.exit(errno.EACCES),
         'files_number_limit': args.files_number_limit if args.files_number_limit > 0 else FILES_NUMBER_LIMIT,
         'language': args.language if args.language in LANGUAGES_ACCEPTED else LANGUAGES_ACCEPTED[0],
-        'extensions': extensions_format(args.extensions),
+        'extensions': format_extensions(args.extensions),
         'words_top_size': args.words_top_size if args.words_top_size > 0 else WORDS_TOP_SIZE,
-        'words_type': args.words_type if args.words_type in WORDS_TYPES_ACCEPTED else WORDS_TYPES_ACCEPTED[0],
-        'syntax_type': args.syntax_type if args.syntax_type in SYNTAX_TYPES_ACCEPTED else SYNTAX_TYPES_ACCEPTED[0],
-        'report_type': args.report_type if args.report_type in REPORTS_TYPES_ACCEPTED else REPORTS_TYPES_ACCEPTED[0],
+        'words_type': args.words_type.upper() if args.words_type in WORDS_TYPES_ACCEPTED else WORDS_TYPES_ACCEPTED[0],
+        'syntax_type': args.syntax_type.upper() if args.syntax_type in SYNTAX_TYPES_ACCEPTED else SYNTAX_TYPES_ACCEPTED[
+            0],
+        'report_type': args.report_type.upper() if args.report_type in REPORTS_TYPES_ACCEPTED else
+        REPORTS_TYPES_ACCEPTED[0],
         'cleanup': args.cleanup,
     }
 
@@ -66,19 +68,20 @@ def main():
     print('-----------------\n')
 
     try:
-        path_temp = directory_temp_create(args['path'])
+        path_temp = create_temp_directory(args['path'])
     except OSError:
         sys.exit('ERROR :: Access denied. Error - {}'.format(errno.EACCES))
 
     repo_path = git_clone(args['git_url'], path_temp)
     code_source = CodeSource(repo_path, args['extensions'], args['files_number_limit'])
-    parser = parser_set(code_source.paths, parsers=PARSERS, language=args['language'])
-
-    tokenizer = TextTokenizer(parser.filter_by(args['syntax_type']))
-    words_statistic = WordsStatistic(tokenizer.filter_by(args['words_type']))
+    parser = parser_set(code_source.paths, filter_list(SYNTAX_TYPES_ACCEPTED, ['ALL', ]),
+                        parsers=PARSERS, language=args['language'])
+    tokenizer = TextTokenizer(parser.filter_names_by(args['syntax_type']), filter_list(WORDS_TYPES_ACCEPTED, ['ALL', ]))
+    words_statistic = WordsStatistic(tokenizer.filter_words_by(args['words_type']))
     words_top = words_statistic.filter_by('top', args['words_top_size'])
     report = Report(words_top, REPORT_FILENAME, TEMPLATE_WORDS_TOP, args['path'])
     report.filter_by(args['report_type'])
+
     print('-----------------\nPROCESSING IS FINISHED')
     if args['cleanup']:
         directory_cleanup(path_temp)
--- code_syntax_analyser/helpers.py
+++ code_syntax_analyser/helpers.py
@@ -21,7 +21,7 @@ class Filters:
     def __init__(self):
         self.filters = {}
 
-    def filter_add(self, name: str, value: any):
+    def add_filter(self, name: str, value: any):
         """
         Add function to self.filters dict
         :param name:
@@ -46,13 +46,13 @@ class Filters:
 
 def git_clone(git_url: str, working_path: str) -> str:
     try:
-        repo = Repo.clone_from(url_check(git_url), path_format(working_path))
-        return path_format(repo.working_dir)
+        repo = Repo.clone_from(check_url(git_url), format_path(working_path))
+        return format_path(repo.working_dir)
     except:
         sys.exit('Error :: Error on clone repository. Check connection or url')
 
 
-def url_check(url: str) -> any:
+def check_url(url: str) -> any:
     try:
         check = urlparse(url)
         return url if all([check.scheme, check.netloc, check.path]) else sys.exit('ValueError :: Enter correct url')
@@ -60,24 +60,24 @@ def url_check(url: str) -> any:
         sys.exit('ValueError :: Enter correct url')
 
 
-def files_paths_get(path: str, files_extensions: list, files_number_limit: int, ) -> list:
+def get_files_paths(path: str, files_extensions: list, files_number_limit: int, ) -> list:
     return list(itertools.islice(
         (p for ext in files_extensions for p in glob.glob('{}**/*{}'.format(path, ext), recursive=True) if
          os.path.isfile(p)), files_number_limit))
 
 
-def path_format(path: str) -> str:
+def format_path(path: str) -> str:
     return path if path.endswith('/') else '{}/'.format(path)
 
 
-def directory_check_exist_and_permissions(path: str):
+def check_directory_exist_and_permissions(path: str):
     return all([os.access(path, os.F_OK), os.access(path, os.R_OK), os.access(path, os.W_OK), os.path.isdir(path)])
 
 
-def directory_temp_create(path: str, suffix: str = ''):
-    path_temp = '{}{}/'.format(path_format(path),
+def create_temp_directory(path: str, suffix: str = ''):
+    path_temp = '{}{}/'.format(format_path(path),
                                string_strip_alphanums(suffix) if len(suffix) > 0 else random_string())
-    if directory_check_exist_and_permissions(path) and not os.path.exists(path_temp):
+    if check_directory_exist_and_permissions(path) and not os.path.exists(path_temp):
         os.makedirs(path_temp)
     else:
         if not directory_cleanup(path_temp):
@@ -86,7 +86,7 @@ def directory_temp_create(path: str, suffix: str = ''):
 
 
 def directory_is_not_empty(path):
-    return True if directory_check_exist_and_permissions(path) and len(os.listdir(path)) > 0 else False
+    return True if check_directory_exist_and_permissions(path) and len(os.listdir(path)) > 0 else False
 
 
 def directory_cleanup(path):
@@ -98,7 +98,7 @@ def directory_cleanup(path):
         return False
 
 
-def extensions_format(s: str) -> list:
+def format_extensions(s: str) -> list:
     return ['.%s' % ext for ext in string_strip_alphanums(s).split(' ') if len(ext) > 0] if len(s) > 0 else ['']
 
 
@@ -106,7 +106,7 @@ def list_values_to_str(list_: list) -> list:
     return list(map(str, list_))
 
 
-def flat_list(l: list) -> list:
+def flatten_list(l: list) -> list:
     return [item for sublist in list(l) for item in sublist]
 
 
@@ -124,3 +124,20 @@ def string_strip_alphanums(s: str):
 
 def random_string(symbols_count: int = 16):
     return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(symbols_count))
+
+
+def filter_tokens_by(tokens: list, key: str, allowed_keys: list) -> list:
+    """
+    Filter
+    :param tokens: List with tokens [['string', 'token_key'], ...], list
+    :param key: String with token key, str
+    :param allowed_keys: List with allowed keys
+    :return: List of words
+    """
+    if key in allowed_keys:
+        return [token[0] for token in tokens if token[1] == key]
+    return [token[0] for token in tokens]
+
+
+def filter_list(list_to_filter: list, filtered_keys: list):
+    return [e for e in list_to_filter if e not in filtered_keys]
